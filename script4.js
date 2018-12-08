49b9d8038590aa00d5aa7b86a6979285759399df
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-08 21:20:04 +0600

    no message

--- script4.js
+++ script4.js
@@ -1,2 +1,17 @@
 
 // Functions
+
+function calculateAge(yearOfBirth){
+    var age = 2016-yearOfBirth;
+    return age;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1969);
+var ageMary = calculateAge(1948);
+console.log(ageJohn);
+console.log(ageMary);
+console.log(ageMike);
+
+
+
