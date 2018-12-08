3bea1a218fb9f09ffd48fc727845fceebd02ca31
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-08 21:13:02 +0600

    no message

--- script3.js
+++ script3.js
@@ -6,3 +6,11 @@ var yearJohn = year-28;
 var yearMark = year -33;
 console.log("John birth year is : " + yearJohn + 
 " and Mark birth year is : "+ yearMark);
+
+var johnOlder = yearJohn < yearMark;
+console.log(johnOlder);
+
+console.log(typeof johnOlder);
+console.log(typeof yearJohn);
+var x;
+console.log(typeof x);
