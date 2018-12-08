091e8eaf31c611ac9a4d7826daff186763a83c24
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-08 23:01:57 +0600

    no message

--- 08 Dec 2018/script7.js
+++ 08 Dec 2018/script7.js
@@ -5,5 +5,38 @@ var john = {
     lastName : "Smith",
     yearOfBirth : 1990,
     job : "Teacher",
-    isMarried : false
+    isMarried : false,
+    family : ["Jame","Mark","Bob"],
+    calculateAge : function(){
+        return 2016 - this.yearOfBirth;
+    }
 }
+
+var age = john.calculateAge();
+john.age = age;
+console.log(john);
+
+var john2 = {
+    name : "John",
+    lastName : "Smith",
+    yearOfBirth : 1990,
+    job : "Teacher",
+    isMarried : false,
+    family : ["Jame","Mark","Bob"],
+    calculateAge : function(){
+        this.age = 2016 - this.yearOfBirth;
+    }
+}
+
+john2.calculateAge();
+console.log(john2);
+
+var mike = {
+    yearOfBirth : 1992,
+    calculateAge : function(){
+        this.age = 2016 - this.yearOfBirth;
+    }
+}
+
+mike.calculateAge();
+console.log(mike);
