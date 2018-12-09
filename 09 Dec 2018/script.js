03e0aeb246bd8b79ccd893a361b0ce06f3d9dd61
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-09 22:08:50 +0600

    no message

---
+++ 09 Dec 2018/script.js
@@ -0,0 +1,23 @@
+
+// Hoisting
+// this means we don't have to make the function at first.
+
+// Functions
+calculateAge(1965);
+
+function calculateAge(year){
+    console.log(2016 - year);
+}
+
+// look this is not working. so, it's only works for function declaration.
+//this is function expression
+//retirement(1990);
+
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
+
+// Variables
+console.log(age);
+var age = 23;
+console.log(age);
