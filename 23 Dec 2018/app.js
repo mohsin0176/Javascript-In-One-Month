b6c2eb861b1078e84c5abfbb7f608e34a40c7a29
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-23 01:05:21 +0600

    no message

--- 23 Dec 2018/app.js
+++ 23 Dec 2018/app.js
@@ -8,12 +8,10 @@ activePlayer = 0;
 //dice = Math.floor(Math.random() * 6) + 1;
 
 // we can use these two methods to dom manipulate
-document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector("#current-" + activePlayer).innerHTML = "<em>"+dice+"</em>";
 // var x = document.querySelector("#score-1").textContent;
 // console.log(x);
-
-document.querySelector(".dice").style.display = "none";
 
 //addEventListener has several overloaded arguments. first one 
 //is here is what type of event it will going to execute.
@@ -30,12 +28,14 @@ document.querySelector(".dice").style.display = "none";
 //     console.log("button clicked inside event listener");
 // });
 
+document.querySelector(".dice").style.display = "none";
+
 document.querySelector(".btn-roll").addEventListener('click',function(){
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
-
     // 2. Display the result
-    document.querySelector(".dice").style.display = "block";
-
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-"+dice+".png";
     // 3. Update the round score if the rolled number was not a 1
 });
