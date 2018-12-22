cb7ab86f029832eab674649f75b5da7a28289d95
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-23 00:34:08 +0600

    no message

--- 23 Dec 2018/app.js
+++ 23 Dec 2018/app.js
@@ -1,5 +1,14 @@
 
-var scores = [0,0];
+var scores, roundScore, activePlayer,dice;
 
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+// we can use these two methods to dom manipulate
+document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).innerHTML = "<em>"+dice+"</em>";
 
 
