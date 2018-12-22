d24a6bdd25f3c72f60228d5597ea885f10a4311f
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-23 00:57:44 +0600

    no message

--- 23 Dec 2018/app.js
+++ 23 Dec 2018/app.js
@@ -1,11 +1,11 @@
 
-var scores, roundScore, activePlayer,dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+//dice = Math.floor(Math.random() * 6) + 1;
 
 // we can use these two methods to dom manipulate
 document.querySelector("#current-" + activePlayer).textContent = dice;
@@ -15,7 +15,27 @@ document.querySelector("#current-" + activePlayer).textContent = dice;
 
 document.querySelector(".dice").style.display = "none";
 
-function btn(){
-    //do something here
-}
-document.querySelector(".btn-roll").addEventListener('click',btn);
+//addEventListener has several overloaded arguments. first one 
+//is here is what type of event it will going to execute.
+//and then, a function to execute something. here is the big
+//deal. we can use an annonymous function in the method 
+//it self, or we can create a function earlier and call it in 
+//the addEventListener method. =D 
+
+// function btn(){
+//     console.log("button clicked");
+// }
+// document.querySelector(".btn-roll").addEventListener('click',btn);
+// document.querySelector(".btn-roll").addEventListener('click',function(){
+//     console.log("button clicked inside event listener");
+// });
+
+document.querySelector(".btn-roll").addEventListener('click',function(){
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    document.querySelector(".dice").style.display = "block";
+
+    // 3. Update the round score if the rolled number was not a 1
+});
