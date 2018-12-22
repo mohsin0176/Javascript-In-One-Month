07101fba7d80b14acc19cbe87f7e23c0cedd06ec
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-23 00:46:43 +0600

    no message

--- 23 Dec 2018/app.js
+++ 23 Dec 2018/app.js
@@ -10,5 +10,12 @@ dice = Math.floor(Math.random() * 6) + 1;
 // we can use these two methods to dom manipulate
 document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector("#current-" + activePlayer).innerHTML = "<em>"+dice+"</em>";
+// var x = document.querySelector("#score-1").textContent;
+// console.log(x);
 
+document.querySelector(".dice").style.display = "none";
 
+function btn(){
+    //do something here
+}
+document.querySelector(".btn-roll").addEventListener('click',btn);
