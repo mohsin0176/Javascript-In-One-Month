9f3196c90d55621a46bd7798befe6f03f503501d
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-25 20:06:17 +0600

    no message

--- 25 Dec 2018/script2.js
+++ 25 Dec 2018/script2.js
@@ -1,16 +1,27 @@
 
 // Functions
 
-var years = [1990,1965,1937,2005,1998];
-function arrayCalc(arr, fn){
+var years = [1990,1965,1937,2005,2000];
+function arrayCalc(arr, fn, isFullAge, maxHeartRate){
     var arrRes = [];
     for(var i=0; i < arr.length; i++){
-        arrRes.push(fn(arr[i]));
+        arrRes.push(fn(arr[i]) + " and this person is adult ? " + isFullAge(fn(arr[i])) + ". And heart rate is " + maxHeartRate(fn(arr[i])));
     }
     return arrRes;
 }
 function calculateAge(el){
     return 2016 - el;
+}
+function isFullAge(el){
+    return el >= 18 ? true : false;
+}
+function maxHeartRate(el){
+    if(el >= 18 && el <= 81){
+        return Math.round(206.9 - (0.67 * el));
+    }    
+    else{
+        return -1;
+    }    
 }
 //here, we are seeing, calculateAge function has no parenthesis
 //this is why, we are not calling this function now
@@ -18,5 +29,5 @@ function calculateAge(el){
 //function. so, you know what the calculateAge function is called
 //right? if you don't know, then remeber, this is called 
 //callback function.
-var ages = arrayCalc(years, calculateAge);
+var ages = arrayCalc(years, calculateAge, isFullAge, maxHeartRate);
 console.log(ages);
