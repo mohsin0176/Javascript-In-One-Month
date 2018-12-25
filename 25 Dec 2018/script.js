d743fd0345c4cc4e1f7bff4620861e13dbee3454
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-25 13:57:16 +0600

    no message

--- 25 Dec 2018/script.js
+++ 25 Dec 2018/script.js
@@ -1,7 +1,33 @@
 // Function constructor
 
-var john = {
-    name : "John",
-    yearOfBirth : 1990,
-    job : "Teacher"    
-};
+// var john = {
+//     name : "John",
+//     yearOfBirth : 1990,
+//     job : "Teacher"    
+// };
+
+var Person = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+
+    this.calculateAge = function(){
+        console.log(this.name + "'s age is : " + (2018 - this.yearOfBirth));
+    }
+}
+
+Person.prototype.anotherCalculateAge = function(){
+    console.log("this is also can be used");
+}
+
+var john = new Person("John",1990,"Teacher");
+
+john.calculateAge();
+
+var jane = new Person("Jane",1969,"Designer");
+var mark = new Person("Mark",1948,"Retired");
+
+jane.calculateAge();
+mark.calculateAge();
+
+mark.anotherCalculateAge();
