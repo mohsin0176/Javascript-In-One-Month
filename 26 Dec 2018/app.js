8a8a03024b1170b3b1f86ab6099b0f6a6d04c44b
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-29 13:12:34 +0600

    no message

--- 26 Dec 2018/app.js
+++ 26 Dec 2018/app.js
@@ -1,15 +1,6 @@
 
 var budgetController = (function(){
-    var x = 23;
 
-    var add = function(a){
-        return x + a;
-    }
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
 })();
 
 var UIController = (function(){
@@ -18,10 +9,5 @@ var UIController = (function(){
 
 
 var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(5);    
-    return {
-        anotherPublic : function(){
-            console.log(z);
-        }
-    }
+    
 })(budgetController,UIController);
