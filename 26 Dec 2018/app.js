f5bff50aa219b0f7b1d397672a9950d367caa4c0
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-29 13:34:17 +0600

    final architecture

--- 26 Dec 2018/app.js
+++ 26 Dec 2018/app.js
@@ -13,9 +13,8 @@ var UIController = (function(){
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    document.querySelector(".add__btn").addEventListener("click",function(){
-        // to do list
-        
+    
+    var ctrlAddItem = function(){
         //1. get the filed input data
 
         //2. Add the item to the budget controller
@@ -25,5 +24,14 @@ var controller = (function(budgetCtrl, UICtrl){
         //4. Calculate the budget
 
         //5. Display the budget on the UI
+        console.log("it works");
+    }
+
+    document.querySelector(".add__btn").addEventListener("click",ctrlAddItem);
+
+    document.addEventListener("keypress",function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();            
+        }
     });
 })(budgetController,UIController);
