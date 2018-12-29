f763b8457d6a553a492d9c28ad31ae05264c8183
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-29 13:17:57 +0600

    to do list in the button click event listener

--- 26 Dec 2018/app.js
+++ 26 Dec 2018/app.js
@@ -13,5 +13,17 @@ var UIController = (function(){
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
+    document.querySelector(".add__btn").addEventListener("click",function(){
+        // to do list
+        
+        //1. get the filed input data
 
+        //2. Add the item to the budget controller
+
+        //3. Add the item to UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+    });
 })(budgetController,UIController);
