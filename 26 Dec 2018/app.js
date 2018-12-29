d633c81b58efb2f22dd7d8735661271000f04a4d
zisan13024 <zisan_cse_mbstu@hotmail.com>
2018-12-29 13:13:47 +0600

    very first modules and architecture of this project

--- 26 Dec 2018/app.js
+++ 26 Dec 2018/app.js
@@ -1,13 +1,17 @@
 
+//BUDGET CONTROLLER
 var budgetController = (function(){
-
+    // Some code
 })();
 
+
+// UI CONTROLLER
 var UIController = (function(){
     // Some code
 })();
 
 
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    
+
 })(budgetController,UIController);
